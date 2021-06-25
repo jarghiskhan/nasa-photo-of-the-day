5453043c402648ed7aa73b9d859e3f6b909ce800
jarghiskhan <jared.l.wyatt@outlook.com>
2021-06-24 21:30:14 -0400

    added styled components and date picker

--- src/components/ImageDiv.js
+++ src/components/ImageDiv.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ImagePuller from "./ImagePuller";
+import DatePicker from "./DatePicker";
 import "../App.css";
+import styled from "styled-components";
 
-function ImageDiv(){
-    return(
-      <div className="image-div">
-        <ImagePuller/>
-      </div>
-    );
-  };
+const ImageDivStyle = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin: auto 2em;
+  text-align: center;
+`;
 
-  export default ImageDiv;
+function DateToFormattedString(d) {         
+                                 
+  var yyyy = d.getFullYear().toString();                                    
+  var mm = (d.getMonth()+1).toString();        
+  var dd  = d.getDate().toString();             
+                       
+  return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+}; 
+
+function ImageDiv() {
+  const [selectedDate, setSelectedDate] = useState(DateToFormattedString(new Date()));
+
+  return (
+    <ImageDivStyle>
+      <ImagePuller selectedDate={selectedDate} />
+      <DatePicker
+        selectedDate={selectedDate}
+        setSelectedDate={setSelectedDate}
+      ></DatePicker>
+    </ImageDivStyle>
+  );
+}
+
+export default ImageDiv;
--- src/components/ImagePuller.js
+++ src/components/ImagePuller.js
@@ -1,20 +1,35 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
 import "../App.css";
+import DatePicker from "./DatePicker";
+
+
+const ImgStyled = styled.img`
+  display: block;
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+  max-height: 90%;
+  margin: 20px auto;
+`;
 
 // `https://api.nasa.gov/planetary/apod?api_key=nHEw9GkSmWftJ9wnOkL1wB289FJUkK0Vqs4JPDbO&date=2021-06-22&thumbs=true
 
-function ImagePuller(){
-    const [pulledImageData, setPulledImageData] = useState([]);
-    useEffect(()=>{
-        axios.get("https://api.nasa.gov/planetary/apod?api_key=nHEw9GkSmWftJ9wnOkL1wB289FJUkK0Vqs4JPDbO&thumbs=true")
-        .then(response => 
-            setPulledImageData(response.data))
-        .catch(error => console.log("Error!", error));
-    },[])
-    return(
-      <img src={pulledImageData.url} alt={pulledImageData.title}></img>
-    );
-  };
+function ImagePuller({selectedDate}) {
+  const [pulledImageData, setPulledImageData] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=nHEw9GkSmWftJ9wnOkL1wB289FJUkK0Vqs4JPDbO&date=${selectedDate}&thumbs=true`
+      )
+      .then((response) => setPulledImageData(response.data))
+      .catch((error) => console.log("Error!", error));
+  }, [selectedDate]);
+  return (
+  <ImgStyled src={pulledImageData.url} alt={pulledImageData.title}></ImgStyled>  
+  )
+}
 
-  export default ImagePuller;
+export default ImagePuller;
